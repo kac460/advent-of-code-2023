7b08d9568ffd7c90e8fedc98ade51fe2c358ab28
Kevin Cadavillo <kac460@rutgers.edu>
2023-12-10 12:24:23 -0500

    Handle edge case, add comments

--- day-8/part_2.py
+++ day-8/part_2.py
@@ -49,18 +49,22 @@ def get_loop_info(
     while True:
         for i in range(len(instructions)):
             # Start of loop discovered:
-            print(curr_location, i, instruction_nums_to_node)
+            # print(curr_location, i, instruction_nums_to_node)
             if in_loop_index is not None and in_loop_index == loop_size:
-                print('Re-looped')
-                return LoopInfo(
+                loop_info = LoopInfo(
                     size=loop_size,
                     first_instruction_num=loop_first_instruction_num,
                     z_positions_in_loop=z_positions
                 )
+                print(f'Re-looped. Loop info: {loop_info}')
+                return loop_info
+            # in theory an optimization might be to store data s.t. we can infer the loop size without re-looping
+            # however, we still need to re-loop to gather the z positions
+            # we *could* also be storing the z positions everytime we see them regardless of if we know we're in a loop or not and then infer the z positions within the loop, but i don't think that's worth it...
             if curr_location in instruction_nums_to_node[i] and loop_first_instruction_num is None:
                 loop_size = instructions_executed - instruction_nums_to_node[i][curr_location]
                 loop_first_instruction_num = instruction_nums_to_node[i][curr_location]
-                print('LOOP DISCOVERED', loop_size, loop_first_instruction_num)
+                print('LOOP DISCOVERED', curr_location, loop_size, loop_first_instruction_num)
                 in_loop_index = 0
                 z_positions = set()
             if in_loop_index is not None and curr_location[-1] == 'Z':
