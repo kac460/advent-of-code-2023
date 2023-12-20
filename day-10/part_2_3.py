8929511e208b5417ce148d015a89bdb603844743
Kevin Cadavillo <kac460@rutgers.edu>
2023-12-19 20:05:58 -0500

    Get a solution that works for the samples but not my input

--- day-10/part_2_3.py
+++ day-10/part_2_3.py
@@ -113,17 +113,27 @@ Figure out whether you have time to search for the nest by calculating the area 
                *X*X*X*
 '''
 # TODO - wait but what about spaces between pipes within the pipe row?
+# Actually, maybe it's fine
 # Directly top/right/left/bottom of space would be another space, not a pipe
 # Top right of space (r, c) is (r-1, c)
 # Top left of space (r, c) is (r-1, c-1)
 # Bottom right of space (r, c) is (r, c)
 # Bottom left of space (r, c) is (r, c-1)
 
-def construct_space_graph(pipe_graph: dict[tuple[int, int], set[tuple[int, int]]], input_grid_rows, input_grid_cols) -> tuple[dict[tuple[int, int], set[tuple[int, int]]], list[list[str]]]:
+from collections import deque
+from part_1 import construct_graph
+
+def get_input_lines(use_sample: bool) -> list[str]:
+    filename = 'sample_2.txt' if use_sample else 'input.txt'
+    with open(f'day-10/{filename}') as f:
+        return [line.strip() for line in f.readlines()]
+
+
+def construct_space_graph(pipe_graph: dict[tuple[int, int], set[tuple[int, int]]], input_grid_rows, input_grid_cols) -> dict[tuple[int, int], set[tuple[int, int]]]:
     space_graph = {
         (r, c): set()
-        for c in range(input_grid_cols) + 1
-        for r in range(input_grid_rows) + 1
+        for c in range(input_grid_cols + 1) 
+        for r in range(input_grid_rows + 1)
     }
     def pipe_neighbors(v):
         return pipe_graph.get(v, set())
@@ -136,14 +146,194 @@ def construct_space_graph(pipe_graph: dict[tuple[int, int], set[tuple[int, int]]
         # TODO - decide how to deal with bounds
         # Maybe just say if we can reach a node that is out of bounds, then it's escapable?
         if top_right not in pipe_neighbors(bottom_right):
-            space_graph[(r, c)].add(r, c+1)
+            space_graph[(r, c)].add((r, c+1))
 
         if top_left not in pipe_neighbors(bottom_left):
-            space_graph[(r, c)].add(r, c-1)
+            space_graph[(r, c)].add((r, c-1))
         
         if top_left not in pipe_neighbors(top_right):
-            space_graph[(r, c)].add(r-1, c)
+            space_graph[(r, c)].add((r-1, c))
 
         if bottom_left not in pipe_neighbors(bottom_right):
-            space_graph[(r, c)].add(r+1, c)
+            space_graph[(r, c)].add((r+1, c))
     return space_graph
+
+
+# Top right of space (r, c) is (r-1, c)
+# Top left of space (r, c) is (r-1, c-1)
+# Bottom right of space (r, c) is (r, c)
+# Bottom left of space (r, c) is (r, c-1)
+
+'''
+               *X*X*X*
+---    ---     
+...            *X*X*X*
+---    ...     
+|||            *X*X*X*
+|||    ---     
+               *X*X*X*
+       |||     
+               *XVXVX*
+       |||    
+               *X*X*X*
+'''
+
+def tile_to_spaces(row: int, col: int) -> tuple[
+    tuple[int, int], 
+    tuple[int, int], 
+    tuple[int, int], 
+    tuple[int, int]
+]:
+    return (
+        (row, col),
+        (row, col+1),
+        (row+1, col),
+        (row+1, col+1)
+    )
+'''
+ 1  procedure BFS(G, root) is
+ 2      let Q be a queue
+ 3      label root as explored
+ 4      Q.enqueue(root)
+ 5      while Q is not empty do
+ 6          v := Q.dequeue()
+ 7          if v is the goal then
+ 8              return v
+ 9          for all edges from v to w in G.adjacentEdges(v) do
+10              if w is not labeled as explored then
+11                  label w as explored
+12                  w.parent := v
+13                  Q.enqueue(w)
+def get_distances(
+    s: tuple[int, int], 
+    graph: dict[tuple[int,int], set[tuple[int,int]]]
+) -> dict[tuple[int, int], int]:
+    dist = 0
+    dists = dict()
+    parents = dict()
+    queue = deque([s])
+    while len(queue) > 0:
+        v = queue.popleft()
+        dists[v] = dists[parents[v]] + 1 if v != s else 0
+        for neighbor in graph[v]:
+            if not dists.get(neighbor):
+                parents[neighbor] = v
+                queue.append(neighbor)
+        dist += 1
+    return dists
+'''
+def update_escapable_tiles(
+    source_row: int, 
+    source_col: int,
+    grid_num_rows: int, 
+    grid_num_cols: int,
+    space_graph: dict[tuple[int, int], set[tuple[int, int]]],
+    escape_grid: list[list[str]],
+    s_loop: set[tuple[int, int]]
+) -> None:
+    spaces = tile_to_spaces(source_row, source_col)
+    escapable = False
+    # print(f'Source: {(source_row, source_col)}')
+    # print(space_graph)
+    for space in spaces:
+        visited = set()
+        queue = deque([space])
+        # print(f'Checking {space}')
+        while len(queue) > 0:
+            # print(len(visited))
+            v = queue.popleft()
+            if v in visited:
+                # print(f'somehow visiting the same node twice {v}')
+                continue
+            visited.add(v)
+            # print(f'v: {v}')
+            if v[_ROW_INDEX] in (-1, grid_num_rows) or v[_COL_INDEX] in (-1, grid_num_cols):
+                continue
+            for neighbor in space_graph[v]:
+                if (
+                    neighbor[0] in (0, grid_num_rows)
+                    or neighbor[1] in (0, grid_num_cols)
+                ):
+                    escapable = True
+                if neighbor not in visited:
+                    queue.append(neighbor)
+
+    for r, c in visited:
+        if (0 <= r < len(escape_grid)) and (0 <= c < len(escape_grid[0])) and (r, c) not in s_loop:
+            escape_grid[r][c] = 'O' if escapable else 'I'
+    print('X')
+
+_ROW_INDEX = 0
+_COL_INDEX = 1
+def node_in_loop_bounds(node: tuple[int, int], loop: set[tuple[int, int]]) -> bool:
+    max_r = max(v[_ROW_INDEX] for v in loop)
+    max_c = max(v[_COL_INDEX] for v in loop)
+    min_r = min(v[_ROW_INDEX] for v in loop)
+    min_c = min(v[_COL_INDEX] for v in loop)
+    node_r, node_c = node
+    return (min_r < node_r < max_r) and (min_c < node_c < max_c)
+
+
+def get_cycle_if_exists(
+    g: dict[tuple[int,int], set[tuple[int,int]]], 
+    source: tuple[int, int],
+) -> set[tuple[int,int]] | None:
+    v = source
+    stack = [v]
+    discovered = set()
+    parent = {
+        v: None
+    }
+    while len(stack) > 0:
+        v = stack.pop()
+        if v not in discovered:
+            discovered.add(v)
+            for neighbor in g.get(v, set()):
+                # back edge (apparently)
+                if neighbor in discovered and parent[v] != neighbor:
+                    # I think given the constraints of the pipes
+                    # the loop is always just everything discovered
+                    return discovered
+                stack.append(neighbor)
+                parent[neighbor] = v
+    return None
+
+
+def main() -> None:
+    input_lines = get_input_lines(use_sample=True)
+    escape_grid = [
+        [input_line[c] for c in range(len(input_line))]
+        for input_line in input_lines
+    ]
+    pipe_graph, start_node, s_node_char = construct_graph(input_lines)
+    s_loop = get_cycle_if_exists(pipe_graph, start_node)
+    space_graph = construct_space_graph(
+        pipe_graph,
+        input_grid_rows=len(input_lines),
+        input_grid_cols=len(input_lines[0])
+    )
+    for r in range(len(input_lines)):
+        for c in range(len(input_lines[0])):
+            if (r, c) not in s_loop and node_in_loop_bounds((r, c), s_loop) and escape_grid[r][c] not in ('I', 'O'):
+                update_escapable_tiles(
+                    r,
+                    c,
+                    len(input_lines),
+                    len(input_lines[0]),
+                    space_graph,
+                    escape_grid,
+                    s_loop
+                )
+                
+    count = 0
+    for line in escape_grid:
+        print(''.join(line))
+        for char in line:
+            if char == 'I':
+                count += 1
+
+    print('FINAL ANSWER:')
+    print(count)
+                    
+if __name__ == '__main__':
+    main()
